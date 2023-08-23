0ea453c5f1514230558edacc91fbd556157ecb0e
apoorvsadana <95699312+apoorvsadana@users.noreply.github.com>
2023-08-23 19:08:35 +0530

    fix: remove unnecessary contracts

--- crates/node/src/constants.rs
+++ crates/node/src/constants.rs
@@ -3,20 +3,17 @@ pub const SHARINGAN_CHAIN_SPEC_URL: &str = "https://raw.githubusercontent.com/ke
 pub const GENESIS_ASSETS_URL: &str =
     "https://raw.githubusercontent.com/keep-starknet-strange/madara/main/configs/genesis-assets/";
 
-pub const GENESIS_ASSETS_FILES: [&str; 5] =
-    ["Account.json", "AccountBaseImpl.json", "CallAggregator.json", "genesis.json", "Proxy.json"];
+pub const GENESIS_ASSETS_FILES: [&str; 1] = ["genesis.json"];
 
 pub const CAIRO_CONTRACTS_URL: &str =
     "https://raw.githubusercontent.com/keep-starknet-strange/madara/main/configs/cairo-contracts/";
 
-pub const CAIRO_CONTRACTS_FILES: [&str; 9] = [
+pub const CAIRO_CONTRACTS_FILES: [&str; 7] = [
     "ArgentAccount.json",
     "ERC20.json",
     "ERC721.json",
     "NoValidateAccount.json",
     "OpenzeppelinAccount.json",
-    "Proxy.json",
-    "test.json",
     "UniversalDeployer.json",
     "cairo_1/NoValidateAccount.casm.json",
 ];
